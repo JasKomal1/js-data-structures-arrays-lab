eb7904c1048b020144fe7411450eee2551e622fd
Jaskomal Randhawa <jaskomal2@gmail.com>
2021-02-20 19:47:43 -0600

    commit tree 7e4e906ca3c247472c0d23eba89d9ca305aa85dd

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let drivers = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendDriver(name) {
+  return drivers.push('Ralph');
+}
+
+function destructivelyPrependDriver(name){
+    return drivers.unshift(name);
+}
+
+function destructivelyRemoveFirstDriver(name){
+    return drivers.shift();
+}
+
+function destructivelyRemoveLastDriver(name) {
+    return drivers.pop();
+}
+
+function appendDriver(name){
+    return [...drivers, name];
+}
+
+function prependDriver(name) {
+    return [name, ...drivers];
+}
+
+function removeLastDriver() {
+    return drivers.slice(0,drivers.length -1);
+}
+
+function removeFirstDriver() {
+    return drivers.slice(1);
+}
